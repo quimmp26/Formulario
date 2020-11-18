94141ce7469afb7a950663644ed7781e49061cf1
quimmp26 <quiim2000@gmail.com>
2020-11-18 16:16:50 +0100

    page bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Componente1Component } from './componente1/componente1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'componente1'
+  },
+  {
+    path: 'componente1', component: Componente1Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
